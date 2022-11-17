72f48feb73113b83058ec4e9ce3829c296c8a339
robbryandev <robbryandev@gmail.com>
2022-11-17 11:36:06 -0800

    Refactor and Add showInfo function

--- src/index.js
+++ src/index.js
@@ -33,33 +33,58 @@ function newEvent(title, imgUrl, place, date, link) {
   return tempCard;
 }
 
-document.querySelector('form').addEventListener("submit", (event) => {
-  event.preventDefault();
+function showError(msg) {
+  return AlertMagic.fire({
+    title: "Oh No!!",
+    text: msg,
+    icon: "error",
+    confirmButtonText: "ok then"
+  });
+}
+
+async function showInfo() {
+  0;
+}
+
+async function showEvents() {
   let city1 = document.getElementById("city").value;
   let state1 = document.getElementById("state").value;
   let eType = document.getElementById("eType").value;
   const searchVar = new Search(city1, state1, eType);
   const events = searchVar.events();
-  events.then((res) => {
-    if (res) {
-      console.log(res);
-      document.getElementById("event-box").removeAll();
-      res.forEach((ev) => {
-        const name = ev.name;
-        const img = ev.images[0].url;
-        const venue = ev._embedded.venues[0].name;
-        const date = ev.dates.start.localDate.split("-");
-        const sales = ev.url;
-        const evCard = newEvent(name, img, venue, date, sales);
-        evCard.appendTo("#event-box");
-      });
-    } else {
-      AlertMagic.fire({
-        title: "Oh No!!",
-        text: "Unknown city or state",
-        icon: "error",
-        confirmButtonText: "ok then"
-      });
-    }
+  return new Promise((resolve) => {
+    events.then((res) => {
+      if (res) {
+        console.log(res);
+        document.getElementById("event-box").removeAll();
+        res.forEach((ev) => {
+          const name = ev.name;
+          const img = ev.images[0].url;
+          const venue = ev._embedded.venues[0].name;
+          const date = ev.dates.start.localDate.split("-");
+          const sales = ev.url;
+          const evCard = newEvent(name, img, venue, date, sales);
+          evCard.appendTo("#event-box");
+        });
+        resolve(true);
+      }
+      resolve(false);
+    });
   });
+}
+
+document.querySelector('form').addEventListener("submit", (event) => {
+  event.preventDefault();
+  showInfo()
+    .then((good) => {
+      if (!good) {
+        showError("An error has occured while fetching news info :(");
+      }
+    });
+  showEvents()
+    .then((good) => {
+      if (!good) {
+        showError("An error has occured while fetching event info :(");
+      }
+    });
 });
